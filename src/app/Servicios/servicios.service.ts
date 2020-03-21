c9e9046124e8248fb55ff1178139763fe9817bd5
Ricardo <ricardo.ang.rmz@gmail.com>
2020-03-21 15:44:07 -0600

    Comunicacion con servicios y agregacion de la fecha

--- src/app/Servicios/servicios.service.ts
+++ src/app/Servicios/servicios.service.ts
@@ -1,24 +1,126 @@
 import { Injectable } from '@angular/core';
+import {AlertController, IonItem} from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiciosService {
-  usuario:any;
-  constructor() {
-    this.usuario={
-      user:'Admin',
-      correo:'admin@utags.edu.mx'
+  showNotas:boolean;
+  changcolor:boolean;
+  sexo: string;
+  strSexo:boolean;
+  blnNext:boolean;
+  arrPersonas: Array<any> = [] as Array<JSON>
+  contact:boolean;
+  fecha:Date;
+
+  regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  regexptelefono = new RegExp(/^([0-9])*$/);
+
+  constructor( public alertController: AlertController ) {
+    this.showNotas = false;
+    this.changcolor = true;
+    this.contactos();
+  }
+  event1(event){
+    this.sexo = event.target.value;
+  }
+
+  sex(){
+    if (this.sexo === 'h') {
+      this.changcolor = true;
+    } else if(this.sexo === 'm') {
+      this.changcolor = false;
     }
   }
 
-  logOut(){
-    this.usuario = {
-      user:"",
-      correo:""
+  contactos(){
+    if (this.arrPersonas.length == 0) {
+      this.contact = false;
+    } else {
+      this.contact = true;
     }
   }
-}
 
-//   constructor() { }
-// }
+  addPerson(strNombre: string, strTelefono: string, strEmail: string,strNotas: string) {
+    this.blnNext = false;
+    (strNombre) ? this.fnError() : this.fnError('Error: Rellene el campo nombre');
+    (strTelefono) ? (this.regexptelefono.test(strTelefono)) ? this.fnError : this.fnError('Error: El campo telefono solo contiene numeros') : this.fnError('Error: Rellene el campo telefono');
+    (strEmail) ? (this.regexp.test(strEmail)) ? this.fnError() :this.fnError('Error: Correo invalido') : this.fnError('Error. Error favor de llenar el campo correo');
+    (strNotas) ? this.fnError() : this.fnError('Error: Rellene el campo notas');
+
+    if(!this.blnNext){
+
+    let strSexo:boolean;
+    strSexo = this.changcolor;
+    const jsnPersona: any = {
+      strNombre,
+      strTelefono,
+      strEmail,
+      strNotas,
+      strSexo
+    };
+    this.presentAlert();
+    this.arrPersonas.push(jsnPersona);
+    this.contactos();
+    }
+  }
+
+  async presentAlert(){
+    const alert = await this.alertController.create({
+      header: 'Éxito',
+      message: 'Se añadio el contacto',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+  async presentAlerterror(msg){
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: msg,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  showNote(){
+    this.showNotas =!this.showNotas;
+  }
+
+  async presentAlertConfirm(item,nombre) {
+    const alert = await this.alertController.create({
+      header: 'Eliminar!',
+      message: 'Desea eliminar <strong> ' + nombre +'? </strong>',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+          }
+        }, {
+          text: 'OK',
+          handler: () => {
+            let a = this.arrPersonas.indexOf(item);
+            this.arrPersonas.splice(a,1);
+            this.contactos();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  fnError(msg?:string){
+    if(msg){
+      this.presentAlerterror(msg);
+      this.blnNext = true;
+    }else if(this.blnNext){
+      this.blnNext = true;
+    }else{
+      this.blnNext = false;
+    }
+  }
+
+  }
+
